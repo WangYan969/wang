ea66e1f61aaf9584425eb20fb694f32438e97353
WangYan969 <2014456750@qq.com>
2018-01-21 22:28:10 +0800

    23

---
+++ exercise02/test01.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+class exercise(object):
+    # def __init__(self,inpoth,outpoth):
+    #     self.inpoth = inpoth
+    #     self.outpoth = outpoth
+    def read(self,inpoth,outpoth):
+        data = open(inpoth, "r")
+        print data.read()
+# e = exercise()
+# e.read('../util/ceshi',"../util/ceshi2")
---
+++ exercise02/test02.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+import exercise02 as e1
+s1 = e1.exercise('s')
+s1.read('../util/ceshi',"../util/ceshi2")
